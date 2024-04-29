c6fe67ff38b2b8932f3d5cc43c0b5bdc2881e27a
apponislame <11appon11@gmail.com>
2024-04-29 11:42:41 +0600

    dynamic title added

---
+++ src/Components/Home/ArtShowCase.jsx
@@ -0,0 +1,14 @@
+const ArtShowCase = () => {
+    return (
+        <div className="container mx-auto">
+            <div className="grid grid-cols-3 my-20">
+                <h1 className="text-4xl text-center my-12 uppercase font-extrabold">Top Art Showcase</h1>
+                <div>
+                    <img src="/SpiritualArt.png" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ArtShowCase;
